9df2b2e2b87299a847907cd2bcbe31284dc3a2f0
Berkin Tosun <berkintsn@gmail.com>
2023-12-28 01:19:29 +0200

    Refactoring the state management

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import server from "./server";
+import { keccak256 } from "ethereum-cryptography/keccak"
+import { utf8ToBytes, toHex } from "ethereum-cryptography/utils"
 
-function Transfer({ address, setBalance }) {
+function Transfer() {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [timestamp, setTimestamp] = useState(Date.now());
+  const [jsonHash, setJsonHash] = useState();
+  const [signature, setSignature] = useState();
+  const [recovery, setRecovery] = useState();
+  
+
+
+  useEffect(() =>{
+    const time = Date.now()
+    setTimestamp(time)
+    setJsonHash(toHex(keccak256(utf8ToBytes(JSON.stringify({recipient,amount: sendAmount,timestamp: time})))))
+  },[sendAmount, recipient])
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
@@ -13,12 +27,8 @@ function Transfer({ address, setBalance }) {
     try {
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
+      } = await server.post(`send`, {recipient,amount: sendAmount,timestamp, signature,recovery});
+      alert(`Transfer completed. ${balance} Balance left `);
     } catch (ex) {
       alert(ex.response.data.message);
     }
@@ -43,6 +53,23 @@ function Transfer({ address, setBalance }) {
           placeholder="Type an address, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+      <label>Sign this Hash = {jsonHash ?? "Fill the form for the hash!"}</label>
+      <label>
+        Hash of your signature
+        <input
+          placeholder="your signature in hash format"
+          value={signature}
+          onChange={setValue(setSignature)}
+        ></input>
+      </label>
+      <label>
+        Recovery bit of your signature
+        <input
+          placeholder="your signatures recovery bit"
+          value={recovery}
+          onChange={setValue(setRecovery)}
         ></input>
       </label>
 
